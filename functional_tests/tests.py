c33945e61bd1cce331c988f0f0447d2757419b66
Charlie <cmdouglas@gmail.com>
2016-01-22 14:59:19 -0600

    hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,25 @@
+import sys
+
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 class NewVisitorTest(StaticLiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -10,7 +27,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def tearDown(self):
         self.browser.quit()
-        
+
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
@@ -19,7 +36,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app.  She goes to check out its
         # homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # She notices the page title and header mention to-do lists.
         self.assertIn('To-Do', self.browser.title)
@@ -42,10 +59,10 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # the page lists:
         # "1. Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
-        
+
         edith_list_url = self.browser.current_url
         self.assertRegex(edith_list_url, r'/lists/.+')
-        
+
         self.check_for_row_in_list_table("1: " + item1)
 
         # There is still a text box inviting her to add another item.  She enters
@@ -58,40 +75,40 @@ class NewVisitorTest(StaticLiveServerTestCase):
         # The page updates again and now shows both items on her list.
         self.check_for_row_in_list_table('1: ' + item1)
         self.check_for_row_in_list_table('2: ' + item2)
-        
+
         # now a new user, Francis, comes along to the site.
-        
+
         ## We use a new browser session to make sure that no information
         ## of Edith's is coming through from cookies etc
         self.browser.quit()
         self.browser = webdriver.Firefox()
-        
+
         # Francis visits the home page.  There is no sign of Edith's list
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
-        
-        # Francis starts a new list by entering a new item. 
+
+        # Francis starts a new list by entering a new item.
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
-        
+
         # Francis gets his own unique URL
         francis_list_url = self.browser.current_url
         self.assertRegex(francis_list_url, r'/lists/.+')
         self.assertNotEqual(francis_list_url, edith_list_url)
-        
+
         # again, there is no trace of Edith's list.
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
-        
+
         # satisfied, they both go back to sleep.
 
     def test_layout_and_styling(self):
         # Edith goes to the home page
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # She notices the input box is nicely centered
@@ -101,7 +118,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
             512,
             delta=5
         )
-        
+
         # She starts a new list and sees the input is nicely
         # centered there too
         inputbox.send_keys('testing\n')
