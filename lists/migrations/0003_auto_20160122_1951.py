16c6bc69d7a4fa4089fc19c52469d8f554f2b4f1
Charlie <cmdouglas@gmail.com>
2016-01-22 13:52:54 -0600

    fix migrations

--- lists/migrations/0003_auto_20160122_1951.py
+++ lists/migrations/0003_auto_20160122_1951.py
@@ -14,7 +14,12 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='List',
             fields=[
-                ('id', models.AutoField(auto_created=True, serialize=False, primary_key=True, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
             ],
+        ),
+        migrations.AddField(
+            model_name='item',
+            name='list',
+            field=models.ForeignKey(default=None, to='lists.List'),
         ),
     ]
