9893ef41da69937cfec06654520e2c6f00707075
Charlie <cmdouglas@gmail.com>
2016-01-26 15:59:57 -0600

    fix FTs

--- functional_tests/test_simple_list_creation.py
+++ functional_tests/test_simple_list_creation.py
@@ -17,7 +17,7 @@ class NewVisitorTest(FunctionalTest):
         self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -39,7 +39,7 @@ class NewVisitorTest(FunctionalTest):
 
         # There is still a text box inviting her to add another item.  She enters
         # "Use peacock feathers to make a fly"
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         item2 = "Use peacock feathers to make a fly"
         inputbox.send_keys(item2)
         inputbox.send_keys(Keys.ENTER)
@@ -62,7 +62,7 @@ class NewVisitorTest(FunctionalTest):
         self.assertNotIn('make a fly', page_text)
 
         # Francis starts a new list by entering a new item.
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.get_item_input_box()
         inputbox.send_keys('Buy milk')
         inputbox.send_keys(Keys.ENTER)
 
--- lists/views.py
+++ lists/views.py
@@ -9,7 +9,7 @@ def home_page(request):
 
 def new_list(request):
     list_ = List.objects.create()
-    item = Item(text=request.POST['item_text'], list=list_)
+    item = Item(text=request.POST['text'], list=list_)
     try:
         item.full_clean()
         item.save()
@@ -17,7 +17,8 @@ def new_list(request):
         list_.delete()
         error = "You can't have an empty list item"
         return render(request, 'home.html', {
-            'error': error
+            'error': error,
+            'form': ItemForm()
         })
     return redirect(list_)
 
@@ -26,7 +27,7 @@ def view_list(request, list_id):
     error = None
     if request.method == "POST":
         try:
-            item = Item(text=request.POST['item_text'], list=list_)
+            item = Item(text=request.POST['text'], list=list_)
             item.full_clean()
             item.save()
             return redirect(list_)
@@ -36,5 +37,6 @@ def view_list(request, list_id):
     
     return render(request, 'list.html', {
         'list': list_,
-        'error': error
+        'error': error,
+        'form': ItemForm()
     })
